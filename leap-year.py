8f262f534f41079ff91011e63e9c11c064f3817e
tnrcelik <99146084+tnrcelik@users.noreply.github.com>
2022-03-29 19:34:27 +0200

    Create leap-year.py

---
+++ leap-year.py
@@ -0,0 +1,16 @@
+# Program will calculate leap year
+
+def leapYear():
+    year = input("Enter a year: ")
+    # check year is leap year or not
+    if(year.isdigit() == True):
+        if( int(year) % 4 == 0 and int(year) %100 != 0 or int(year) % 400 == 0 ):
+       # if ( ):
+            print ("This is a leap-year!")
+
+        else:
+            print ("This is not a leap-year!")
+    else :
+        print("Please Enter valid Year ,digit only!")
+
+leapYear()
